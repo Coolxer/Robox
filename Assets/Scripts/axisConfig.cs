28fd241edd228854bea6327eab9fc57899325894
coolxer <lukasik4789@gmail.com>
2020-07-13 08:53:24 +0200

    place robot, pivot points, set axes

---
+++ Assets/Scripts/axisConfig.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class axisConfig : MonoBehaviour
+{
+    public GameObject axis;
+
+    public Vector3 vector;
+    public float min;
+    public float max;
+    private float speed = 50.0f;
+
+    private float lastSliderValue;
+
+    private bool auto = false;
+
+    private int direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderController.minValue = min;
+        sliderController.maxValue = max;
+
+         sliderController.onValueChanged.AddListener(
+             delegate { onSlideRotate();}
+        );
+
+        speedController.minValue = 0.0f;
+        speedController.value = this.speed;
+        speedController.maxValue = 100.0f;
+
+        speedController.onValueChanged.AddListener(
+             delegate { setSpeed();}
+        );
+
+        speedText.text = this.speed.ToString();
+       
+        autoButton.onClick.AddListener(setAutoRotate);
+
+        lastSliderValue = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!auto) return;
+
+        float newValue = lastSliderValue + (this.speed * Time.deltaTime * direction);
+        float delta = newValue - lastSliderValue;
+        transform.Rotate(vector * delta);
+
+        sliderController.value = newValue;
+
+        if(newValue >= max && direction == 1) direction = -1;
+        else if(newValue <= min && direction == -1) direction = 1;
+
+        posText.text = newValue.ToString();
+
+        lastSliderValue = newValue;
+    }
+
+    public void onSlideRotate()
+    {
+        float currentValue = sliderController.value;
+
+        float delta = currentValue - lastSliderValue;
+
+        transform.Rotate (vector * delta);
+
+        posText.text = currentValue.ToString();
+
+        lastSliderValue = currentValue;
+    }
+
+    public void setAutoRotate()
+    {
+        string text = auto ? "AUTO" : "STOP";
+
+        autoButton.GetComponentInChildren<Text>().text = text;
+
+        auto = !auto;
+        
+        lastSliderValue = lastSliderValue == 0 ? 0.01f : lastSliderValue;
+    }
+
+    public void setSpeed()
+    {
+        this.speed = speedController.value;
+        speedText.text = this.speed.ToString();
+    }
+}
---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public GameObject target;
+
+    private Vector3 lastPosition;
+
+    public float speed = 100.0f;
+
+    private float angleH = 0, angleV = 0;
+
+    private float distance = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        distance += Input.GetAxis("Mouse ScrollWheel");
+
+        transform.RotateAround(target.transform.position, Vector3.up, -Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+
+/*
+        if(moveHorizontal != 0) 
+            angleH += moveHorizontal;
+
+        if(moveVertical != 0)
+            angleV += moveVertical;
+
+        Vector3 tmp;
+        tmp.x = Mathf.Cos(angleH * (Mathf.PI / 180)) * Mathf.Sin(angleV * (Mathf.PI / 180)) * distance + target.transform.position.x;
+        tmp.z = Mathf.Sin(angleH * (Mathf.PI / 180)) * Mathf.Sin(angleV * (Mathf.PI / 180)) * distance + target.transform.position.z;
+        tmp.y = Mathf.Sin(angleV * (Mathf.PI / 180)) * distance + target.transform.position.y;
+        transform.position = Vector3.Slerp(transform.position, tmp, speed * Time.deltaTime);
+        transform.LookAt(target.transform);
+
+        */
+
+        /*
+        if(Input.GE)       
+            lastPosition = camera.ScreenToViewportPoint(Input.mousePosition);
+   
+        else if(Input.GetMouseButton(0))
+        {
+            Vector3 direction = lastPosition - camera.ScreenToViewportPoint(Input.mousePosition);
+
+            camera.transform.position = new Vector3();
+
+            camera.transform.Rotate(Vector3.right, direction.y * 180);
+            camera.transform.Rotate(Vector3.down, direction.y * 180, Space.World);
+
+            camera.transform.Translate(new Vector3(0 ,0, -10));
+
+            lastPosition = camera.ScreenToViewportPoint(Input.mousePosition);
+        }
+        */
+    }
+}
