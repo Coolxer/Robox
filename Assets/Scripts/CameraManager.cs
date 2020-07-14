5080bccaf591884c98dd63f64548ca7dd731343a
coolxer <lukasik4789@gmail.com>
2020-07-14 10:09:36 +0200

    camera system

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour
+{
+    public Button camButton;
+
+    private int currentCameraIndex = 0;
+    private int lastCameraIndex = 0;
+    private Camera[] cameras = new Camera[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            cameras[i] = transform.Find("cam" + i.ToString()).GetComponent<Camera>();
+            cameras[i].enabled = false;
+        }
+
+        cameras[currentCameraIndex].enabled = true;
+
+        camButton.onClick.AddListener(changeCamera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void changeCamera()
+    {
+        lastCameraIndex = currentCameraIndex;
+
+        if(currentCameraIndex == 4)
+            currentCameraIndex = 0;
+        else
+            currentCameraIndex++;
+
+        cameras[lastCameraIndex].enabled = false;
+        cameras[currentCameraIndex].enabled = true;
+    }
+}
--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -8,6 +8,9 @@ public class CameraMovement : MonoBehaviour
     public float zoomspeed = 1000.0f;
     public float rotatespeed = 100.0f;
 
+    private Vector3 lastPos = Vector3.zero;
+    private Vector3 lastDelta = Vector3.zero;
+
     void Start()
     {
         transform.LookAt(target.transform.position);
@@ -16,12 +19,33 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(target.transform.position, Vector3.down, Input.GetAxis("Horizontal") * rotatespeed * Time.deltaTime);
- 
-        if (Input.mouseScrollDelta.y > 0)
+        rotate();
+        zoom(); 
+    }
+
+    private void rotate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 delta = Input.mousePosition - lastPos;
+
+             if(Vector3.Dot(transform.up, Vector3.up) >= 0)
+                target.transform.Rotate(target.transform.up, -Vector3.Dot(delta, transform.right), Space.World);
+            else
+                target.transform.Rotate(target.transform.up, Vector3.Dot(delta, transform.right), Space.World);
+        
+            target.transform.Rotate(transform.right, Vector3.Dot(delta, transform.up), Space.World);
+        }
+
+        lastPos = Input.mousePosition;
+    }
+
+    private void zoom()
+    {
+        if (Input.mouseScrollDelta.y > 0 && transform.position.z <= 500.0f)
             transform.position += transform.forward * zoomspeed * Time.deltaTime;
  
-        if (Input.mouseScrollDelta.y < 0)
+        if (Input.mouseScrollDelta.y < 0 && transform.position.z >= -1800.0f)
             transform.position -= transform.forward * zoomspeed * Time.deltaTime;
     }
 }
