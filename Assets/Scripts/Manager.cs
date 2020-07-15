26f4fd8ab7366a34a3428fc71f3aec5ade6a9707
coolxer <lukasik4789@gmail.com>
2020-07-15 10:10:13 +0200

    create buttons actions

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Manager : MonoBehaviour
 {
+    public Button restartBtn;
+
+    public Button showHideBtn;
+
+    public Button exitBtn;
+
+    public GameObject leftPanel;
+    public GameObject rightPanel;
+    
+    private bool visibleSides = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        restartBtn.onClick.AddListener(restart);
+        showHideBtn.onClick.AddListener(showHide);
+        exitBtn.onClick.AddListener(exit);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void restart() 
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+
+    private void showHide() 
+    {
+        visibleSides = !visibleSides;
+
+        leftPanel.SetActive(visibleSides);
+        rightPanel.SetActive(visibleSides);
+
+        string text = showHideBtn.GetComponentInChildren<Text>().text;
+        showHideBtn.GetComponentInChildren<Text>().text = visibleSides ? "HIDE" : "SHOW";
+    }
+
+    private void exit()
+    {
+        Application.Quit();
     }
 }
