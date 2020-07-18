fbbe31400d1566cc195e89bd06a923acee081e49
coolxer <lukasik4789@gmail.com>
2020-07-18 08:57:11 +0200

    comment valueControlller

--- Assets/Scripts/ValueController.cs
+++ Assets/Scripts/ValueController.cs
@@ -3,63 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// klasa reprezentujaca kontroler wartosci
+// zarzadzanie wartoscia polega na sledzeniu wartosci Slider'a (suwaka)
+// oraz aktualizowanie wyswietlanej wartosci w polu
 public class ValueController : MonoBehaviour
 {
+    // obiekt typu Slider reprezentujacy suwak umozliwiajacy zmiane wartosci od min do max
     private Slider slider;
+
+    // obiekt typu Text to pole, w ktorym wyswietlana jest aktualna wartosci suwaka
     private Text text;
+
+    // ostatnia wartosc ustawiona jest na malo znaczaca 0.0001f, tak aby mozliwy byl prawidlowy start
     private float lastValue = 0.0001f;
+
+    // roznica pomiedzy aktualna i poprzednia wartosci
     private float delta = 0;
 
+    // funkcja uruchamiana podczas ladowania skryptu
     void Awake()
     {
+        // pobranie slider'a sposrod komponentow obiektu
         slider = GetComponentInChildren<Slider>();
+
+        // pobranie text'u o nazwie "value" sposrod komponentow obiektu
         text = transform.Find("value").GetComponent<Text>();
     }
 
-    // Start is called before the first frame update
+    // funkcja jest uruchamiana przed pierwszym update'm
     void Start()
     {
+        // dodanie funkcji obslugujacej zmiane wartosci suwaka
         slider.onValueChanged.AddListener(
             delegate { setValue(slider.value);}
         );
     }
 
+    // funkcja zwracajaca aktualna wartosc suwaka
     public float getValue()
     {
         return slider.value;
     }
 
+    // funkcja zwracajaca poprzednia wartosc
     public float getLastValue()
     {
         return lastValue;
     }
 
+    // funkcja zwracajaca roznice miedzy aktualna i poprzednia wartoscia
     public float getDelta()
     {
         return delta;
     }
 
+    // funkcja resetujaca delte
     public void resetDelta()
     {
         delta = 0;
     }
 
+    // funkcja zwracajaca minimalna wartosc suwaka
     public float getMin()
     {
         return slider.minValue;
     }
 
+    // funkcja zwracajaca maksymalna wartosc suwaka
     public float getMax()
     {
         return slider.maxValue;
     }
 
+    // funkcja uruchamiana podczas zmiany wartosci suwaka, a takze wywolywana z "zewnatrz"
     public void setValue(float value)
     {
+        // ustawienie wartosci suwaka na podana wartosc
         slider.value = value;
+
+        // zmiana wartosci pola tekstowego
         text.text = value.ToString();
 
+        // obliczenie delty
         delta = value - lastValue;
+
+        // zapisanie aktualnej wartosci jako poprzednia
         lastValue = value;
     }
 }
