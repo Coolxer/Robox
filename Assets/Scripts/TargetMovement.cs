906a7b3420c318f49068b0fc116785d24c7d8758
coolxer <lukasik4789@gmail.com>
2020-07-20 09:09:06 +0200

    clean scene

---
+++ Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    public Transform center;
+
+    public Vector3 vector;
+
+    public float speed = 20.0f;
+
+    private float angleH = 0;
+    private float angleV = 0;
+
+    public GameObject blueAxis;
+
+    public GameObject greenAxis;
+
+    private Vector3 vb;
+    private Vector3 vg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vb = Vector3.up;
+        vg = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.LookAt(center, vb);
+            if(angleH <= 165.0f)
+            {
+                float value = speed * Time.deltaTime;
+                transform.RotateAround(center.position, vb, value);
+                blueAxis.transform.RotateAround(center.position, vb, value);
+                //vg = Quaternion.AngleAxis(value, vb).eulerAngles;
+                angleH += value;
+            }
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.LookAt(center, vb);
+            if(angleH >= -165.0f)
+            {
+                float value = speed * Time.deltaTime;
+                transform.RotateAround(center.position, -vb, value);
+                blueAxis.transform.RotateAround(center.position, -vb, value);
+                //vg = Quaternion.AngleAxis(-value, vb).eulerAngles;
+                angleH -= value;
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.LookAt(center, vg);
+            if(angleV <= 55.0f)
+            {
+                float value = speed * Time.deltaTime;
+                transform.RotateAround(center.position, vg, value);
+                greenAxis.transform.RotateAround(center.position, vg, value);
+                //vb = Quaternion.AngleAxis(value, vg).eulerAngles;
+                angleV += value;
+            }
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.LookAt(center, vg);
+            if(angleV >= -80.0f)
+            {
+                float value = speed * Time.deltaTime;
+                transform.RotateAround(center.position, -vg, value);
+                greenAxis.transform.RotateAround(center.position, -vg, value);
+                //vb = Quaternion.AngleAxis(-value, vg).eulerAngles;
+                angleV -= value;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WireArc.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class WireArc : MonoBehaviour
+{
+    public GameObject center;
+    public Vector3 vector;
+
+    public float startAngle;
+    public float endAngle;
+    public float radius;
+    public int segments;
+
+    private LineRenderer line;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+
+        line.positionCount =  segments;
+        line.useWorldSpace = false;
+
+        line.startWidth = line.endWidth = 20.0f;
+
+        line.transform.position = center.transform.position;
+
+        calculatePoints();
+    }
+
+    void calculatePoints()
+    {
+        float angle = startAngle;
+        float arcLength = endAngle - startAngle;
+
+        for (int i = 0; i < segments; i++)
+        {
+            float x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            float y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+            if(vector == Vector3.right)
+                line.SetPosition (i,new Vector3(x , y, 0));
+            else if(vector == Vector3.up)
+                line.SetPosition (i,new Vector3(x, 0, y));
+
+            angle += (arcLength / segments);
+        }
+    }
+}
